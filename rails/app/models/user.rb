3072dbdf23d8e091d5c707a33d7544e94f841a09
yuta20253 <magee.20253@gmail.com>
2026-01-11 02:55:22 +0900

    Userモデルの更新

--- rails/app/models/user.rb
+++ rails/app/models/user.rb
@@ -12,15 +12,30 @@
 #  remember_created_at    :datetime
 #  name                   :string(100)
 #  name_kana              :string(100)
-#  user_role_id           :bigint           not null
+#  user_role_id           :bigint
 #  jti                    :string(255)      not null
 #  deleted_at             :datetime
 #  created_at             :datetime         not null
 #  updated_at             :datetime         not null
+#  high_school_id         :bigint
+#  address_id             :bigint
 #
 class User < ApplicationRecord
   belongs_to :user_role, optional: true
-  has_one :user_personal_info
+  belongs_to :address, optional: true
+
+  has_one :user_personal_info, dependent: :destroy
+  has_one :high_school, dependent: :destroy
+  has_one :user_overall_question_stat, dependent: :destroy
+
+  has_many :goals, dependent: :destroy
+  has_many :tasks, dependent: :destroy
+  has_many :study_logs, dependent: :destroy
+  has_many :reflections, dependent: :destroy
+  has_many :question_histories, dependent: :destroy
+  has_many :user_subject_question_stats, dependent: :destroy
+  has_many :subjects, through: :user_subject_question_stats
+  has_many :user_unit_question_stats, dependent: :destroy
 
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
