fb10ae65e83e03e50723479ed28f4d1b55fe6b1f
yuta20253 <magee.20253@gmail.com>
2026-01-11 18:17:34 +0900

    認証周りのルーティング

--- rails/config/routes.rb
+++ rails/config/routes.rb
@@ -1,5 +1,21 @@
 Rails.application.routes.draw do
-  devise_for :users
+  devise_for :users,
+    controllers: {
+      sessions: "users/sessions",
+      registrations: "users/registrations",
+      passwords: "users/passwords",
+      confirmations: "users/confirmations",
+      unlocks: "users/unlocks"
+    }
+
+    devise_scope :users do
+      post "/admin/login",  to: "admin/sessions#create"
+      delete "/admin/logout", to: "admin/sessions#destroy"
+      post "/teacher/login",  to: "teacher/sessions#create"
+      post "/student/login",  to: "student/sessions#create"
+      post "/parent/login",  to: "parent/sessions#create"
+    end
+
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
