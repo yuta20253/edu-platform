6f35b46de7ea1bf84605b25c7cc74ccd8688231a
yuta20253 <magee.20253@gmail.com>
2026-01-15 00:29:35 +0900

    ポートの追加

--- rails/config/initializers/cors.rb
+++ rails/config/initializers/cors.rb
@@ -7,7 +7,7 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins "localhost:3000", "127.0.0.1:3000"
+    origins "localhost:3000", "localhost:3001", "localhost:3002", "127.0.0.1:3000"
 
     resource "*",
       headers: :any,
