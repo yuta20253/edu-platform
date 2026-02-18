30c32fb87d669562a2712b7bfea7f707d8378802
yuta20253 <magee.20253@gmail.com>
2026-02-19 01:48:09 +0900

    メソッドチェーンの間違え修正

--- rails/app/controllers/api/v1/student/courses_controller.rb
+++ rails/app/controllers/api/v1/student/courses_controller.rb
@@ -5,7 +5,7 @@ module Api
     module Student
       class CoursesController < Api::V1::Student::BaseController
         def index
-          courses = CoursesQuery.new.includes.result
+          courses = CoursesQuery.new.includes_units.result
           if params[:subject].present?
             courses = CoursesQuery.new.join_subject.includes_units.by_subject(params[:subject]).result
           end
