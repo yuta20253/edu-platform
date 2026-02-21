dcbb300901dcf1fdcb5f08b45e0753e9a811267d
yuta20253 <magee.20253@gmail.com>
2026-02-21 22:06:02 +0900

    目標一覧取得機能実装

--- rails/app/controllers/api/v1/student/goals_controller.rb
+++ rails/app/controllers/api/v1/student/goals_controller.rb
@@ -4,6 +4,12 @@ module Api
   module V1
     module Student
       class GoalsController < Api::V1::Student::BaseController
+        def index
+          goals = GoalsQuery.new(current_user.goals).due_soon.paginate(page: params[:page],
+                                                                       per_page: params[:per_page] || 10).result
+          render json: goals, each_serializer: GoalSerializer, status: :ok
+        end
+
         def create
           form = ::Student::CreateGoalForm.new(current_user: current_user, **create_goal_params.to_h.symbolize_keys)
 
