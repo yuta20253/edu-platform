5334c304d13d457f57d7b73e9aded46de092c1f9
yuta20253 <magee.20253@gmail.com>
2026-02-24 01:55:53 +0900

    テスト

--- rails/app/forms/auth/password_reset_form.rb
+++ rails/app/forms/auth/password_reset_form.rb
@@ -1,11 +1,16 @@
-class Auth::PasswordResetForm
-  include ActiveModel::Model
-  include ActiveModel::Attributes
-  include ActiveModel::Validations
+# frozen_string_literal: true
 
-  attr_accessor :reset_password_token, :password, :password_confirmation
+module Auth
+  class PasswordResetForm
+    include ActiveModel::Model
+    include ActiveModel::Attributes
+    include ActiveModel::Validations
 
-  validates :reset_password_token, presence: true
-  validates :password, presence: true
-  validates :password_confirmation, presence: true
+    attr_accessor :reset_password_token, :password, :password_confirmation
+
+    validates :reset_password_token, presence: true
+    validates :password, presence: true
+    validates :password_confirmation, presence: true
+    validates :password, confirmation: true
+  end
 end
