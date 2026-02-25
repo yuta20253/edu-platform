b07cde9a32df8e4b2b90c17935cb6fbc2e185c7e
yuta20253 <magee.20253@gmail.com>
2026-02-26 00:34:21 +0900

    テストの追加とrubocop

--- rails/spec/services/auth/change_password_service_spec.rb
+++ rails/spec/services/auth/change_password_service_spec.rb
@@ -1,10 +1,12 @@
+# frozen_string_literal: true
+
 require 'rails_helper'
 
 RSpec.describe Auth::ChangePasswordService do
-  describe "#call" do
+  describe '#call' do
     let(:user) { create(:user) }
 
-    context "tokenが有効な場合" do
+    context 'tokenが有効な場合' do
       let(:raw_token) { user.send_reset_password_instructions }
       let(:form) do
         Auth::PasswordResetForm.new(
@@ -14,13 +16,13 @@ RSpec.describe Auth::ChangePasswordService do
         )
       end
 
-      it "パスワードを更新できる" do
+      it 'パスワードを更新できる' do
         service = described_class.new(form)
-        expect(service.call).to eq "パスワードを更新しました。"
+        expect(service.call).to eq 'パスワードを更新しました。'
       end
     end
 
-    context "tokenが無効な場合" do
+    context 'tokenが無効な場合' do
       let(:form) do
         Auth::PasswordResetForm.new(
           reset_password_token: 'invalid',
@@ -29,7 +31,7 @@ RSpec.describe Auth::ChangePasswordService do
         )
       end
 
-      it "ValidationErrorをraiseする" do
+      it 'ValidationErrorをraiseする' do
         service = described_class.new(form)
         expect { service.call }.to raise_error(ValidationError)
       end
