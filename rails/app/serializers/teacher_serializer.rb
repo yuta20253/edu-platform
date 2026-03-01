3ea6599537be0e0799f66feec790a43b3b63dee5
yuta20253 <magee.20253@gmail.com>
2026-03-02 00:26:15 +0900

    TeacherPermissionSerializer作成

--- rails/app/serializers/teacher_serializer.rb
+++ rails/app/serializers/teacher_serializer.rb
@@ -4,4 +4,5 @@ class TeacherSerializer < ActiveModel::Serializer
   attributes :id, :name, :name_kana, :grade
 
   belongs_to :grade, serializer: GradeSerializer
+  has_one :teacher_permission, serializer: TeacherPermissionSerializer
 end
