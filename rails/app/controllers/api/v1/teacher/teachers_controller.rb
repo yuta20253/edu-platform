980d36217b7317fccd14f0a89771979d11dd2bf1
yuta20253 <magee.20253@gmail.com>
2026-03-10 01:29:09 +0900

    リファクタリング

--- rails/app/controllers/api/v1/teacher/teachers_controller.rb
+++ rails/app/controllers/api/v1/teacher/teachers_controller.rb
@@ -5,13 +5,20 @@ module Api
     module Teacher
       class TeachersController < Api::V1::Teacher::BaseController
         def index
-          teachers = TeachersQuery.new(current_user.high_school.users).colleagues.result
+          teachers = teachers_scope
           render json: teachers, each_serializer: TeacherSerializer, status: :ok
         end
 
         def show
-          teacher = TeachersQuery.new(current_user.high_school.users).colleagues.find(params[:id])
+          teacher = teachers_scope.find(params[:id])
           render json: teacher, serializer: TeacherSerializer, status: :ok
+        end
+
+        private
+
+        def teachers_scope
+          query = ::Teacher::TeachersQuery.new(current_user.high_school.users).colleagues
+          query.result
         end
       end
     end
