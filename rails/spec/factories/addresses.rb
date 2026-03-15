6fc3f267e6a9d6263767e3e7ff50d26e2a802320
yuta20253 <magee.20253@gmail.com>
2026-03-15 18:58:28 +0900

    rubocop

--- rails/spec/factories/addresses.rb
+++ rails/spec/factories/addresses.rb
@@ -1,9 +1,11 @@
+# frozen_string_literal: true
+
 FactoryBot.define do
   factory :address do
-    postal_code { "1000001" }
-    prefecture { "東京都" }
-    city { "千代田区" }
-    town { "千代田" }
-    street_address { "1-1-1" }
+    postal_code { '1000001' }
+    prefecture { '東京都' }
+    city { '千代田区' }
+    town { '千代田' }
+    street_address { '1-1-1' }
   end
 end
