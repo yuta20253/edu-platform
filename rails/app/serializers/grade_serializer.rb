cabb67e0112154ea58d0c06feadf0f6a3654568a
yuta20253 <magee.20253@gmail.com>
2026-03-19 01:23:55 +0900

    rubocop

--- rails/app/serializers/grade_serializer.rb
+++ rails/app/serializers/grade_serializer.rb
@@ -1,3 +1,15 @@
+# frozen_string_literal: true
+
+# == Schema Information
+#
+# Table name: grades
+#
+#  id             :bigint           not null, primary key
+#  high_school_id :bigint           not null
+#  year           :integer          not null
+#  created_at     :datetime         not null
+#  updated_at     :datetime         not null
+#
 class GradeSerializer < ActiveModel::Serializer
   attributes :id, :year, :display_name
 end
