664d79db1cce38364a2a4d41ac64edce0c599402
yuta20253 <magee.20253@gmail.com>
2026-03-20 03:10:42 +0900

    grades_controllerも新規作成時は認証スキップ

--- rails/app/controllers/api/v1/grades_controller.rb
+++ rails/app/controllers/api/v1/grades_controller.rb
@@ -3,6 +3,8 @@
 module Api
   module V1
     class GradesController < ApplicationController
+      skip_before_action :authenticate_user!
+
       def index
         grades = Grade.where(high_school_id: params[:high_school_id])
         render json: grades, each_serializer: GradeSerializer, status: :ok
