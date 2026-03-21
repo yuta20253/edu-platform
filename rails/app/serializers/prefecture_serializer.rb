33d07a024f117fca2a22855f4c9f98eda062734b
yuta20253 <magee.20253@gmail.com>
2026-03-22 03:54:26 +0900

    PrefectureSerializerにアノテーション追加

--- rails/app/serializers/prefecture_serializer.rb
+++ rails/app/serializers/prefecture_serializer.rb
@@ -1,5 +1,14 @@
 # frozen_string_literal: true
 
+# == Schema Information
+#
+# Table name: prefectures
+#
+#  id         :bigint           not null, primary key
+#  name       :string(255)      not null
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#
 class PrefectureSerializer < ActiveModel::Serializer
   attributes :id, :name
 end
