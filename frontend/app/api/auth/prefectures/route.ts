d7791af4166f3bbfc8e02b55528be36e45d724a2
yuta20253 <magee.20253@gmail.com>
2026-03-21 18:21:10 +0900

    引数削除

--- frontend/app/api/auth/prefectures/route.ts
+++ frontend/app/api/auth/prefectures/route.ts
@@ -1,7 +1,7 @@
 import { railsFetch } from "@/libs/server/rails/railsFetch";
 import { NextResponse } from "next/server";
 
-export async function GET(req: Request) {
+export async function GET() {
   try {
     const { data, status } = await railsFetch("/api/v1/prefectures", {
       method: "GET",
