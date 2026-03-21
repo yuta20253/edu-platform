a5e896042725b632686d14b59319348d27126976
yuta20253 <magee.20253@gmail.com>
2026-03-21 15:52:34 +0900

    high_school_factoryにprefectureの情報追加

--- rails/spec/factories/high_schools.rb
+++ rails/spec/factories/high_schools.rb
@@ -4,13 +4,15 @@
 #
 # Table name: high_schools
 #
-#  id         :bigint           not null, primary key
-#  name       :string(50)       not null
-#  created_at :datetime         not null
-#  updated_at :datetime         not null
+#  id            :bigint           not null, primary key
+#  name          :string(50)       not null
+#  created_at    :datetime         not null
+#  updated_at    :datetime         not null
+#  prefecture_id :bigint
 #
 FactoryBot.define do
   factory :high_school do
     sequence(:name) { |n| "テスト高校#{n}" }
+    prefecture { Prefecture.find_or_create_by(name: '東京都') }
   end
 end
