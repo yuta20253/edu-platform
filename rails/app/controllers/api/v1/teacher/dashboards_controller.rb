5519da53209c15e2b03e4ffc867a70292c56f8b8
yuta20253 <magee.20253@gmail.com>
2026-03-27 00:35:06 +0900

    高１〜３年生でサービスを利用している学年別生徒数

--- rails/app/controllers/api/v1/teacher/dashboards_controller.rb
+++ rails/app/controllers/api/v1/teacher/dashboards_controller.rb
@@ -5,7 +5,21 @@ module Api
     module Teacher
       class DashboardsController < Api::V1::Teacher::BaseController
         def show
+          # 利用している所属高校の生徒
+          students = current_user.high_school.users
 
+          # 利用している所属高校の生徒数(高１〜３年生のみ)
+          grade_counts = students.joins(:grade).group('grades.year').count
+
+          # お知らせ(直近10件くらい)
+
+          # 昨日学習した生徒数(高１〜３年生のみ)
+
+          render json: {
+            grade_1: grade_counts[1] || 0,
+            grade_2: grade_counts[2] || 0,
+            grade_3: grade_counts[3] || 0
+          }, status: :ok
         end
       end
     end
