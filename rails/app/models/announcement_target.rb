c4c579c1bd33913b9a6e16e8a5fc07a7c6386069
yuta20253 <magee.20253@gmail.com>
2026-03-28 05:44:10 +0900

    モデルの作成

---
+++ rails/app/models/announcement_target.rb
@@ -0,0 +1,20 @@
+# == Schema Information
+#
+# Table name: announcement_targets
+#
+#  id              :bigint           not null, primary key
+#  announcement_id :bigint           not null
+#  user_role_id    :bigint
+#  grade_id        :bigint
+#  high_school_id  :bigint
+#  user_id         :bigint
+#  created_at      :datetime         not null
+#  updated_at      :datetime         not null
+#
+class AnnouncementTarget < ApplicationRecord
+  belongs_to :announcement
+  belongs_to :user_role, optional: true
+  belongs_to :grade, optional: true
+  belongs_to :school, optional: true
+  belongs_to :user, optional: true
+end
