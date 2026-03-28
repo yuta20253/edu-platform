845df9c8e4f3701eac42d6e433493795a14dcd76
yuta20253 <magee.20253@gmail.com>
2026-03-29 01:19:54 +0900

    リレーションの修正

--- rails/app/models/announcement_target.rb
+++ rails/app/models/announcement_target.rb
@@ -17,6 +17,6 @@ class AnnouncementTarget < ApplicationRecord
   belongs_to :announcement
   belongs_to :user_role, optional: true
   belongs_to :grade, optional: true
-  belongs_to :school, optional: true
+  belongs_to :high_school, optional: true
   belongs_to :user, optional: true
 end
