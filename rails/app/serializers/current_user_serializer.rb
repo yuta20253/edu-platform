bbf0dd65ff70034768911fa598c00f5b193940a1
yuta20253 <magee.20253@gmail.com>
2026-04-09 17:18:53 +0900

    リレーション関係の修正とuser_personal_info_serializerの作成

--- rails/app/serializers/current_user_serializer.rb
+++ rails/app/serializers/current_user_serializer.rb
@@ -3,9 +3,9 @@
 class CurrentUserSerializer < ActiveModel::Serializer
   attributes :id, :name, :name_kana, :email, :profile_completed
 
+  has_one :user_personal_info, serializer: UserPersonalInfoSerializer
   belongs_to :user_role
   belongs_to :high_school
-  belongs_to :user_personal_info
 
   def profile_completed
     object.profile_completed?
