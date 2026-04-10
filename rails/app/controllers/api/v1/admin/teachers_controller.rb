1e5c222670d0fb8c5b3c9405a1de52bdc8a3e447
centerRyo <abc.atrn@gmail.com>
2026-04-10 17:25:32 +0900

    feat(admin/teachers): create アクションを実装

--- rails/app/controllers/api/v1/admin/teachers_controller.rb
+++ rails/app/controllers/api/v1/admin/teachers_controller.rb
@@ -15,6 +15,42 @@ module Api
             )
           }
         end
+
+        def create
+          school = HighSchool.find(params[:high_school_id])
+
+          ActiveRecord::Base.transaction do
+            teacher_role = UserRole.find_by!(name: 'teacher')
+            password = SecureRandom.hex(16)
+            user = User.create!(
+              name: teacher_params[:name],
+              email: teacher_params[:email],
+              password: password,
+              password_confirmation: password,
+              user_role: teacher_role,
+              high_school: school
+            )
+            user.create_teacher_permission!(grade_scope: :own_grade, manage_other_teachers: false)
+            user.send_reset_password_instructions
+
+            user.reload
+            render json: { teacher: ::Admin::TeacherSerializer.new(user) }, status: :created
+          end
+        rescue ActiveRecord::RecordInvalid => e
+          render json: { errors: e.record.errors.full_messages }, status: :unprocessable_entity
+        end
+
+        def update
+        end
+
+        private
+
+        def teacher_params
+          params.require(:teacher).permit(
+            :name, :email, :password, :password_confirmation,
+            :grade_scope, :manage_other_teachers, grade_ids: []
+          )
+        end
       end
     end
   end
