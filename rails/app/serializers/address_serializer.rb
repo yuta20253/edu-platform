8763c774e4b24b8961f6da3be9b3c242d662cc2e
やすす <yuta20253@yasusunoMacBook-Pro.local>
2026-04-16 02:51:31 +0900

    AddressSerializerの作成

---
+++ rails/app/serializers/address_serializer.rb
@@ -0,0 +1,23 @@
+# == Schema Information
+#
+# Table name: addresses
+#
+#  id             :bigint           not null, primary key
+#  postal_code    :string(8)        not null
+#  city           :string(50)       not null
+#  town           :string(50)
+#  street_address :string(100)
+#  deleted_at     :datetime
+#  created_at     :datetime         not null
+#  updated_at     :datetime         not null
+#  prefecture_id  :bigint
+#
+class AddressSerializer < ActiveModel::Serializer
+  attributes :id, :postal_code, :city, :town, :prefecture
+
+  belongs_to :prefecture, serializer: PrefectureSerializer
+
+  def prefecture
+    object.prefecture&.slice(:id, :name)
+  end
+end
