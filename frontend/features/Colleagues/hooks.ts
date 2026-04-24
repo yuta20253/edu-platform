a31980fb7392ba3538b01d6a81738802a9e3790f
やすす <yuta20253@yasusunoMBP.lan>
2026-04-25 02:06:48 +0900

    importの修正

--- frontend/features/Colleagues/hooks.ts
+++ frontend/features/Colleagues/hooks.ts
@@ -1,7 +1,7 @@
 "use client";
 
 import { apiClient } from "@/libs/http/apiClient";
-import { useRouter } from "next/router";
+import { useRouter } from "next/navigation";
 import { useState, useEffect } from "react";
 import { TeachersData } from "./types";
 
