e8294204001eb284164d71f2bb4499bb7ac4dac7
やすす <yuta20253@yasusunoMacBook-Pro.local>
2026-04-27 02:30:07 +0900

    確認画面用API作成(Question毎にQuestionHistoryが存在する場合は必要データを紐付けて返す・全問返却するのでQuestionHistoryが存在しない場合もありうる)

--- rails/app/controllers/api/v1/student/confirmations_controller.rb
+++ rails/app/controllers/api/v1/student/confirmations_controller.rb
@@ -5,12 +5,20 @@ module Api
     module Student
       class ConfirmationsController < Api::V1::Student::BaseController
         def show
-          question_histories = current_user.question_histories.where(task_id: confirmation_params[:task_id],
-                                                                     unit_id: confirmation_params[:unit_id])
+          questions = Question.where(unit_id: confirmation_params[:unit_id])
 
-          return render json: { errors: '解答履歴がありません' }, status: :not_found unless question_histories
+          question_histories = current_user
+                               .question_histories
+                               .where(task_id: confirmation_params[:task_id], unit_id: confirmation_params[:unit_id])
+                               .includes(:question_choice)
+                               .index_by(&:question_id)
 
-          render json: question_histories, status: :ok
+          return render json: { errors: '解答履歴がありません' }, status: :not_found if question_histories.empty?
+
+          render json: questions,
+                 each_serializer: QuestionConfirmationSerializer,
+                 question_histories_by_question_id: question_histories,
+                 status: :ok
         end
 
         private
---
+++ rails/app/serializers/question_confirmation_serializer.rb
@@ -0,0 +1,31 @@
+# frozen_string_literal: true
+
+class QuestionConfirmationSerializer < ActiveModel::Serializer
+  attributes :question_id, :question_text, :correct_answer, :selected_choice_number, :status
+
+  def question_id
+    object.id
+  end
+
+  delegate :question_text, to: :object
+
+  delegate :correct_answer, to: :object
+
+  def selected_choice_number
+    history&.question_choice&.choice_number
+  end
+
+  def status
+    history.present? ? 'answered' : 'unanswered'
+  end
+
+  private
+
+  def history
+    @history ||= question_histories_by_question_id[object.id]
+  end
+
+  def question_histories_by_question_id
+    instance_options[:question_histories_by_question_id] || {}
+  end
+end
