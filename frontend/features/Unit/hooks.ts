54b224bc0456a077be8df5271fb41c9eba4b3214
やすす <yuta20253@yasusunoMBP.lan>
2026-05-20 17:43:45 +0900

    問題スタート画面作成

---
+++ frontend/features/Unit/hooks.ts
@@ -0,0 +1,46 @@
+"use client";
+
+import { apiClient } from "@/libs/http/apiClient";
+import { useRouter } from "next/navigation";
+import { useEffect, useState } from "react";
+
+type Props = {
+  taskId: number;
+  unitId: number;
+};
+
+type UnitType = {
+  id: number;
+  course_id: number;
+  unit_name: string;
+  course: {
+    id: number;
+    level_number: number;
+    level_name: string;
+  };
+};
+
+export const useGetUnit = ({ taskId, unitId }: Props) => {
+  const [unit, setUnit] = useState<UnitType | null>(null);
+  const [loading, setLoading] = useState<boolean>(false);
+  const [error, setError] = useState<boolean>(false);
+  const router = useRouter();
+
+  useEffect(() => {
+    apiClient
+      .get<UnitType>(`/api/student/tasks/${taskId}/units/${unitId}`)
+      .then((res) => setUnit(res.data))
+      .catch((err) => {
+        if (err.response?.status === 401) {
+          router.push("/login");
+          return;
+        }
+
+        setError(true);
+        setUnit(null);
+      })
+      .finally(() => setLoading(false));
+  }, [router, taskId, unitId]);
+
+  return { unit, loading, error };
+};
---
+++ frontend/features/Unit/index.tsx
@@ -0,0 +1,49 @@
+"use client";
+
+import { Box, CircularProgress } from "@mui/material";
+import { useGetUnit } from "./hooks";
+import { Presenter } from "./Presenter";
+
+type Props = {
+  // goalId?: number;
+  taskId: number;
+  unitId: number;
+};
+
+export const Unit = ({ taskId, unitId }: Props) => {
+  const { unit, loading, error } = useGetUnit({ taskId, unitId });
+
+  if (loading) {
+    return (
+      <Box
+        sx={{
+          display: "flex",
+          justifyContent: "center",
+          alignItems: "center",
+          height: "100vh",
+        }}
+      >
+        <CircularProgress />
+      </Box>
+    );
+  }
+
+  if (!unit || error) {
+    return (
+      <Box
+        sx={{
+          display: "flex",
+          justifyContent: "center",
+          alignItems: "center",
+          height: "100%",
+          flexDirection: "column",
+          gap: 1,
+        }}
+      >
+        データの取得に失敗しました
+      </Box>
+    );
+  }
+
+  return <Presenter unit={unit} />;
+};
