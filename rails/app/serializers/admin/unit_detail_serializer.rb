ed3590ee158e42edf58da8f6dc27194c623cfd89
centerRyo <abc.atrn@gmail.com>
2026-05-21 14:19:09 +0900

    Unit#show で recent_import_histories を返す実装 (Green)
    unit.import_histories.order(started_at: :desc).limit(5) を ImportHistorySerializer で返す。

--- rails/app/serializers/admin/unit_detail_serializer.rb
+++ rails/app/serializers/admin/unit_detail_serializer.rb
@@ -4,6 +4,8 @@ module Admin
   class UnitDetailSerializer < ActiveModel::Serializer
     attributes :id, :course_id, :unit_name, :questions, :recent_import_histories
 
+    RECENT_IMPORT_HISTORIES_LIMIT = 5
+
     def questions
       ActiveModelSerializers::SerializableResource.new(
         object.questions.order(:id),
@@ -12,7 +14,10 @@ module Admin
     end
 
     def recent_import_histories
-      []
+      ActiveModelSerializers::SerializableResource.new(
+        object.import_histories.order(started_at: :desc).limit(RECENT_IMPORT_HISTORIES_LIMIT),
+        each_serializer: ::ImportHistorySerializer
+      ).as_json
     end
   end
 end
