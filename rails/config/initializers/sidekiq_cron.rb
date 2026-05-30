6604b59dbc3c02571dd5defcfe4e988501f4b249
やすす <yuta20253@yasusunoMacBook-Pro.local>
2026-05-30 22:19:41 +0900

    sidekiq_cronの設定修正

--- rails/config/initializers/sidekiq_cron.rb
+++ rails/config/initializers/sidekiq_cron.rb
@@ -1,5 +1,9 @@
-schedule_file = Rails.root.join("config/schedule.yml")
+if Sidekiq.server? && ENV["ENABLE_SIDEKIQ_CRON"] == "true"
+  schedule_file = Rails.root.join("config/schedule.yml")
 
-if File.exist?(schedule_file)
-  Sidekiq::Cron::Job.load_from_hash YAML.load_file(schedule_file)
+  if File.exist?(schedule_file)
+    Sidekiq::Cron::Job.load_from_hash(
+      YAML.load_file(schedule_file)
+    )
+  end
 end
