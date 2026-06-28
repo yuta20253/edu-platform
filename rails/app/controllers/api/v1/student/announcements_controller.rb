620572cfbc5b871eda44168da0c60d78c78e2bcb
やすす <yuta20253@yasusunoMBP.lan>
2026-06-28 16:32:22 +0900

    生徒のお知らせ一覧取得機能API

---
+++ rails/app/controllers/api/v1/student/announcements_controller.rb
@@ -0,0 +1,34 @@
+# frozen_string_literal: true
+
+module Api
+  module V1
+    module Student
+      class AnnouncementsController < Api::V1::Student::BaseController
+        def index
+          announcements = announcement_scope
+                          .order(published_at: :desc, id: :desc)
+                          .page(params[:page])
+                          .per(20)
+
+          render json: {
+            announcements: ActiveModelSerializers::SerializableResource.new(
+              announcements, each_serializer: AnnouncementSerializer
+            ),
+            meta: {
+              current_page: announcements.current_page,
+              total_pages: announcements.total_pages,
+              total_count: announcements.total_count,
+              per_page: 20
+            }
+          }
+        end
+
+        private
+
+        def announcement_scope
+          Announcement.for_user(current_user).includes(:publisher).published
+        end
+      end
+    end
+  end
+end
