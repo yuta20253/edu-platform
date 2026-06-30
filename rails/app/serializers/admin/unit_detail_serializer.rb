4dc57348f11b5a534c61dfae8e5c8249fc6f93c7
centerRyo <abc.atrn@gmail.com>
2026-06-30 15:13:44 +0900

    feat: 単元詳細APIに講座情報を追加
    UnitDetailSerializer に course(id/subject/level_name/level_number)を追加し、
    コントローラで course を eager load。フロントのパンくずで講座名を表示するため。

--- rails/app/serializers/admin/unit_detail_serializer.rb
+++ rails/app/serializers/admin/unit_detail_serializer.rb
@@ -2,9 +2,19 @@
 
 module Admin
   class UnitDetailSerializer < ActiveModel::Serializer
-    attributes :id, :course_id, :unit_name, :questions, :recent_import_histories
+    attributes :id, :course_id, :unit_name, :course, :questions, :recent_import_histories
 
     RECENT_IMPORT_HISTORIES_LIMIT = 5
+
+    def course
+      course = object.course
+      {
+        id: course.id,
+        subject: { id: course.subject.id, name: course.subject.name },
+        level_name: course.level_name,
+        level_number: course.level_number
+      }
+    end
 
     def questions
       ActiveModelSerializers::SerializableResource.new(
