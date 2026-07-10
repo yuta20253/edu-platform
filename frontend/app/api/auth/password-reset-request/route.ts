181a43d8cc57cea43fa9758617b1a837b13336c8
やすす <yuta20253@yasusunoMacBook-Pro.local>
2026-07-11 04:32:18 +0900

    Prettier適用

--- frontend/app/api/auth/password-reset-request/route.ts
+++ frontend/app/api/auth/password-reset-request/route.ts
@@ -2,24 +2,28 @@ import { apiFetch } from "@/libs/server/apiFetch";
 import { NextResponse } from "next/server";
 
 export async function POST(req: Request) {
-    const body = await req.json();
-    const email = body.email;
+  const body = await req.json();
+  const email = body.email;
 
-    if (!email) {
-        return NextResponse.json(
-            { errors: ["メールアドレスを入力してください"] },
-            { status: 422 },
-        );
-    }
+  if (!email) {
+    return NextResponse.json(
+      { errors: ["メールアドレスを入力してください"] },
+      { status: 422 },
+    );
+  }
 
-    const receivedRequest = await apiFetch(`/api/v1/password/reset/request`,{
-        method: "POST",
-        body: JSON.stringify({email}),
+  const receivedRequest = await apiFetch(`/api/v1/password/reset/request`, {
+    method: "POST",
+    body: JSON.stringify({ email }),
+  });
+
+  if (!receivedRequest.response.ok) {
+    return NextResponse.json(receivedRequest.data, {
+      status: receivedRequest.response.status,
     });
+  }
 
-    if (!receivedRequest.response.ok) {
-        return NextResponse.json(receivedRequest.data, { status: receivedRequest.response.status });
-    }
-
-    return NextResponse.json(receivedRequest.data, { status: receivedRequest.response.status });
+  return NextResponse.json(receivedRequest.data, {
+    status: receivedRequest.response.status,
+  });
 }
