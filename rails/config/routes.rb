f117e927f10b3693c5f255b05e796f18a512e7fc
やすす <yuta20253@yasusunoMBP.lan>
2026-07-19 17:09:36 +0900

    ルーティング追加

--- rails/config/routes.rb
+++ rails/config/routes.rb
@@ -41,6 +41,7 @@ Rails.application.routes.draw do
           end
         end
         resources :courses, only: :index
+        resources :analytics, only: :index
       end
 
       namespace :teacher do
