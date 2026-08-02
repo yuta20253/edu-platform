98fc334fb5e328e928282d91e4955678ae1cf5d8
やすす <yuta20253@yasusunoMacBook-Pro.local>
2026-08-02 17:31:18 +0900

    教員一覧画面で送信状況を表示できるように修正

--- rails/app/serializers/teacher_serializer.rb
+++ rails/app/serializers/teacher_serializer.rb
@@ -1,8 +1,13 @@
 # frozen_string_literal: true
 
 class TeacherSerializer < ActiveModel::Serializer
-  attributes :id, :name, :name_kana, :grade
+  attributes :id, :name, :name_kana, :grade, :invitation_status
 
   belongs_to :grade, serializer: GradeSerializer
   has_one :teacher_permission, serializer: TeacherPermissionSerializer
+
+  def invitation_status
+    latest = object.received_teacher_notifications.order(sent_at: :desc).limit(1).first
+    latest ? latest.status : 'not_sent'
+  end
 end
