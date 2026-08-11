e86d8d0c322e59c753d7a95e598cbe08a7d92210
やすす <yuta20253@yasusunoMBP.lan>
2026-08-12 01:19:01 +0900

    ルーティング修正

--- rails/config/routes.rb
+++ rails/config/routes.rb
@@ -54,7 +54,8 @@ Rails.application.routes.draw do
         resources :teacher_notifications
         resources :teacher_notification_results
         resources :grades, only: :index
-        resources :school_classes
+        resources :school_classes, only: [:index, :show]
+        resources :school_class_requests, only: [:create, :update]
         resource :dashboard, only: :show
       end
 
